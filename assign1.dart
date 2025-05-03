094f80aad098e6b1d7a8149db72f9d19e117e762
usman-shah12 <usmanshah216@gmail.com>
2025-05-03 16:36:59 +0500

    print list names

---
+++ assign1.dart
@@ -0,0 +1,15 @@
+//Q.1) Create a list of names and print all names using list.
+
+void main() {
+  List student = [
+    "usman",
+    "ubaid",
+    "hamza",
+    "rafay",
+    "rehman",
+    "zain",
+    "sameer",
+  ];
+
+  print("the total students are $student");
+}
