4e21a8fc42c8c3f7663772c0999937bfcbf4da29
usman-shah12 <usmanshah216@gmail.com>
2025-05-03 19:20:08 +0500

    print-list-of-name-class-rollno

---
+++ assign2.dart
@@ -0,0 +1,27 @@
+//Q. 3)  create a list  of name, class, roll no, grade, percentage. And print.
+void main() {
+  List students = [
+    {
+      "name": "usman",
+      "class": "7th",
+      "rollno": "678",
+      "grade": "A+",
+      "percentage": "87%",
+    },
+    {
+      "name": "ahmed",
+      "class": "8th",
+      "rollno": "578",
+      "grade": "B",
+      "percentage": "89%",
+    },
+    {
+      "name": "sumair",
+      "class": "9th",
+      "rollno": "478",
+      "grade": "A",
+      "percentage": "85%",
+    },
+  ];
+  print(students);
+}
